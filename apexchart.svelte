7ac8a1dbd0094e8f76a82f7c84121ebd187a161c
decoder.id <75539501+decoderid@users.noreply.github.com>
2025-04-06 18:30:36 +0700

    Create apexchart.svelte

---
+++ apexchart.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { ApexOptions } from 'apexcharts'
+  import { onMount } from 'svelte'
+  let { options } = $props<{ options: ApexOptions }>()
+  let chartContainer: HTMLDivElement
+
+  onMount(async () => {
+    const ApexCharts = (await import('apexcharts')).default
+    const chart = new ApexCharts(chartContainer!, options)
+    chart.render()
+  })
+</script>
+
+<div bind:this={chartContainer}></div>
